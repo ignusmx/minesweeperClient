af6a05359e77c485f853b1abefa459b932c73658
Christian Læirbag <laeirbag@gmail.com>
2022-04-06 17:58:35 -0600

    FirstCommint (have no idea what I'm doing).

---
+++ project/src/index.web.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render
+(
+	<React.StrictMode>
+		{/* Here will be our codes and components */}
+	</React.StrictMode>,
+	document.getElementById('root')
+);
